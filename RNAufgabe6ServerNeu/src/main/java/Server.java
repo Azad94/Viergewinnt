d02f00b695fb0cd7b224a1e055d9e1aeb2c4400b
Sven Marquardt <Sven2q@hotmail.de>
2015-11-02 04:13:45 +0100

    Aufgabe nochmals versucht diesmal mit seinem

---
+++ RNAufgabe6ServerNeu/src/main/java/Server.java
@@ -0,0 +1,152 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by sven on 02.11.15.
+ */
+public class Server {
+
+    private static final int BUFSIZERECEIVE = 11;
+    private static InetAddress address;
+
+    public static void main(String args[]){
+        if (args.length != 2) {
+            System.err.println("Bitte geben sie Port und filepath ein");
+            System.exit(-1);
+        } else {
+            int port = Integer.valueOf(args[0]);
+            String filePath = args[1];
+            // Open special datagramm socket from jlibcnds library, do not change this
+            javax.net.DatagramSocket dtgSock;
+            try {
+                dtgSock = new javax.net.DatagramSocket(port);
+                serverRoutine(filePath,dtgSock,port);
+                dtgSock.close();
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    /**
+     *Empfängt einen String
+     * @param maxBytes          Die maximalgröße des empfangenen Packets
+     * @return                  Ein String der den Inhalt des Packets repräsentiert
+     * @throws SocketException  Wenn eine verbindung fehlschlägt.
+     */
+    public static byte[] receive(int maxBytes, javax.net.DatagramSocket socket) throws IOException {
+        DatagramPacket packet = new DatagramPacket(new byte[maxBytes],maxBytes);
+        socket.setSoTimeout(10000);
+        socket.receive(packet);
+        address = packet.getAddress();
+        if (packet.getLength()==0) return new byte[0];
+        return packet.getData();
+    }
+
+
+
+    /**
+     * Sende einen Strig an den angegebenen Host
+     * @param s             String der zu senden ist.
+     * @throws IOException  Wirdt eine Exception wenn das senden fehlschlägt
+     */
+    public static void send(String s, javax.net.DatagramSocket socket, int port) throws IOException {
+        byte[] bytes = s.getBytes();
+        DatagramPacket packet = new DatagramPacket(bytes,bytes.length,address,port);
+        socket.send(packet);
+
+    }
+
+
+    /**
+     * Ist für die eigentliche Client Logik zuständig heißt ein und ausgabe
+     * @param port  Port für das senden und empfangen von daten
+     * @param file  Die Datei in die wir schreiben werden
+     */
+    private static void serverRoutine(String file,javax.net.DatagramSocket socket,int port) {
+        boolean failure;
+        List<String> list;
+        try {
+            list = new LinkedList<>();
+            byte[] bytes=new byte[0];
+            int packageNumber=0;
+            while (true){
+                try {
+                    bytes=receive(BUFSIZERECEIVE,socket);
+                    failure=false;
+                    if (bytes.length==0) break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    failure=true;
+                }
+                if ((packageNumber=checkFailure(bytes))>=0&&!failure){
+                    if (packageNumber==list.size()) {
+                        System.out.println("Hinzugefügt");
+                        list.add(encodeData(bytes));
+                    }
+                    send(0b10000001 + "",socket, port);
+                }else{
+                    send(0b01111110+"",socket, port);
+                }
+                Thread.sleep(500);
+            }
+            writeData(list,file);
+
+        } catch (InterruptedException  | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Schreibt die daten in eine Datei
+     *
+     * @param data Die daten die geschrieben werden sollen
+     */
+    private static void writeData(List<String> data, String filePAth) {
+        try {
+            Writer writer = new FileWriter(new File(filePAth));
+            for (String s : data) {
+                writer.append(s);
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Encodiert den Rahmen heißt die flags werden aus dem Rahmen entnommen
+     *
+     * @param data Der Rahmen der Encodiert wird
+     * @return Den Rahmen ohne Flags
+     */
+    private static String encodeData(byte[] data) {
+        StringBuilder builder = new StringBuilder(data.length-3);
+        for (int i = 0; i < data.length-4; i++) {
+            builder.append(data[i]);
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Überprüft ob ein Rahmen nicht richtig übersendet wurde in dem es nur die Flag bytes überprüft
+     *
+     * @param bytes         Der Rahmen der überprüft wird
+     * @return Die nummer des Packets oder 0 wenn das Packet einen fehler enthält
+     */
+    private static int checkFailure(byte[] bytes) {
+        boolean check = false;
+        int flagBegin = bytes.length - 3;
+        if(((int) bytes[flagBegin]) !=(byte) 0b01111110) check = true;
+        if(((int) bytes[flagBegin+1]) !=(byte) flagBegin) check = true;
+
+        return check?-1:((int)bytes[bytes.length - 1]);
+    }
+}
